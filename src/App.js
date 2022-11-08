4ce06c1a4ed1c4649c63b691ee79e97042764ba5
odeta <odeta.kizyte@gmail.com>
2022-11-08 20:32:33 +0700

    Add styling to header

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import { Link, Outlet, useLocation } from 'react-router-dom';
 import './App.css';
 
 function App() {
+  const location = useLocation()
+
   return (
     <div className="App">
       <header className="App-header">
+        <h1 className="App-logo">Photos</h1>
 
+        {location.pathname !== '/photos' && (
+          <Link className="Link" to='/photos'>Back to all photos</Link>
+        )}
       </header>
 
       <main className="App-main">
